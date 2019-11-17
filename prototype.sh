7688082c8838ac2135eeffc1c27e1dcba82c8ebb
Tianchen Jin <tianchenjin@gmail.com>
2019-11-17 14:57:15 -0800

    Added warning threshold and bad latency in a separate log.

--- prototype.sh
+++ prototype.sh
@@ -1,24 +1,32 @@
 pingAndLog()
 {
     pingResult=$(ping $1 -n 1 | grep Reply)
-    pingResultWithTimestamp="${pingResult} time=\"$(date -R)\""
-    echo ${pingResultWithTimestamp} | tee -a $2
+    pingResultWithTimestamp="$pingResult time=\"$(date -R)\""
+    echo $pingResultWithTimestamp | tee -a $2
+
+    latency=$(echo $pingResult | cut -d: -f2 | cut -d' ' -f3 | cut -d= -f2 | tr -d ms)
+    if [ $latency -gt $3 ]; then
+        echo $pingResultWithTimestamp >> $4
+    fi
 }
 
 
 loopIntervalInSeconds=10
 logFilename=latency.log
+warnThresholdInMiliseconds=20
+warnLogFilename=badLatency.log
 
 echo "Press <Enter> anytime to stop"
+echo "Latency warning threshold is ${warnThresholdInMiliseconds}ms"
 
 doLoop=true
 while ${doLoop}
 do
-    pingAndLog 8.8.8.8 ${logFilename}
-    pingAndLog 192.168.1.1 ${logFilename}
+    pingAndLog 8.8.8.8 $logFilename $warnThresholdInMiliseconds $warnLogFilename
+    pingAndLog 192.168.1.1 $logFilename $warnThresholdInMiliseconds $warnLogFilename
 
     # Press enter to escape loop
-    read -p "" -t ${loopIntervalInSeconds}
+    read -p "" -t $loopIntervalInSeconds
     if [ $? -le 128 ]; then # did not timeout
         doLoop=false
     fi

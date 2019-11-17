f046212ffc6fab8348586e9daa5709efd94c8735
Tianchen Jin <tianchenjin@gmail.com>
2019-11-17 13:18:21 -0800

    Refactored code. Ping local gateway and public ip at the same time."

--- prototype.sh
+++ prototype.sh
@@ -1,14 +1,21 @@
-loopNum=5
-loopIntervalInSeconds=2
-doLoop=true
+pingAndLog()
+{
+    pingResult=$(ping $1 -n 1 | grep Reply)
+    pingResultWithTimestamp="${pingResult} time=\"$(date -R)\""
+    echo ${pingResultWithTimestamp} | tee -a $2
+}
+
+
+loopIntervalInSeconds=10
+logFilename=latency.log
 
 echo "Press <Enter> anytime to stop"
 
+doLoop=true
 while ${doLoop}
 do
-    pingResult=$(ping 8.8.8.8 -n 1 | grep Reply)
-    pingResultWithTimestamp="${pingResult} time=\"$(date -R)\""
-    echo ${pingResultWithTimestamp} | tee -a latency.log
+    pingAndLog 8.8.8.8 ${logFilename}
+    pingAndLog 192.168.1.1 ${logFilename}
 
     # Press enter to escape loop
     read -p "" -t ${loopIntervalInSeconds}
